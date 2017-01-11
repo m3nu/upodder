3ceeb988b8c3f88e2b5eda9b10f4ded7528ea2fb
Manuel <manu@snapdragon.cc>
2017-01-11 06:02:48 +0000

    Bump version 0.6.8 > 0.6.9 [skip ci]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         name='upodder',
         author='"Stan Vitkovskiy, Manuel Riel"',
         author_email='stas.vitkovsky[at]gmail.com, manu[at]vlx.cc',
-        version='0.6.8',
+        version='0.6.9',
         license='LICENSE.txt',
         url='https://github.com/manuelRiel/upodder',
         description=description,
